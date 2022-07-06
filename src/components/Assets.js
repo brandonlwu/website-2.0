435580c0a549e5a049613f1b2d9cf65a51a4dec0
Brandon Wu <brandonlwu@Brandons-MBP-2.cable.rcn.com>
2022-07-05 23:43:56 -0400

    keysho updates

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -9,6 +9,11 @@ import projects from '../assets/projects.png'
 function Assets() {
     return(
         <div>
+            <a href = "https://twitter.com/wuhoobrandon"><img className = "twitter social-icons" src = {book}></img></a>
+            <a href = "https://twitter.com/wuhoobrandon"><img className = "twitter social-icons" src = {home}></img></a>
+            <a href = "https://twitter.com/wuhoobrandon"><img className = "twitter social-icons" src = {connect}></img></a>
+            <a href = "https://twitter.com/wuhoobrandon"><img className = "twitter social-icons" src = {projects}></img></a>
+            <a href = "https://twitter.com/wuhoobrandon"><img className = "twitter social-icons" src = {camera}></img></a>
             <img src = {book}/>
             <img src = {home}/>
             <img src = {connect}/>
