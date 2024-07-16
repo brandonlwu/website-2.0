92627175419bb689fd917ae4037a68b4d1e219c3
Brandon Wu <brandonlwu@Brandons-MacBook-Pro-10.local>
2024-07-16 17:51:10 -0400

    delete

--- src/components/Condensed.js
+++ src/components/Condensed.js
@@ -36,9 +36,6 @@ function Condensed() {
                         </span>, run 
                         <span className = "nfme link">
                         <a href = "https://letsnft.me/" target="_blank"> letsnft.me</a>
-                        </span>, and map my thoughts on
-                        <span className = "everything link">
-                        <a href = "https://everythingiwishitoldyou.com/" target="_blank"> everythingiwishitoldyou</a>
                         </span>
                     </li>
                     <li>In the past, I worked at
